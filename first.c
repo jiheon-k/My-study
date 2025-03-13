8057767713495dc2f6bcee3355101f6e81dea9e4
jiheon <joey3792@naver.com>
2025-03-13 18:13:05 +0900

    병합정렬

--- first.c
+++ first.c
@@ -1,6 +1,56 @@
+//merge sort 구현하기
+
 #include<stdio.h>
+
+void merge(int arr[], int left, int mid, int right){
+    int i,j,k;
+    int n1=mid-left+1, n2=right-mid;
+    int L[n1], R[n2];
+    for(i=0; i<n1; i++)
+        L[i]=arr[left+i];
+    for(j=0; j<n2; j++) 
+        R[j]=arr[mid+j+1];
+
+    i=0, j=0, k=left;
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            arr[k]=L[i];
+            i++;
+        }
+        else{
+            arr[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k]=L[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        arr[k]=R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right){
+    int mid=(left + right)/2;
+    if(left<right){
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
 int main(void){
-    int a=10;
-    for(int i=1; i<10; i++)
-        printf("%d*%d = %d\n", i, a, i*a);
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++)
+        scanf("%d", &arr[i]);
+    mergeSort(arr, 0, n-1);
+    for(int i=0; i<n; i++)
+        printf("%d\n", arr[i]);
 }
